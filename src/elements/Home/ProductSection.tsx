3022f14ec5ec76b31a86fce66a73a6e608b256ff
Dayanan Chaudhary <daychy72@gmail.com>
2026-08-31 16:41:58 +0545

    style: reduce font size of reward coins badge in ProductSection

--- src/elements/Home/ProductSection.tsx
+++ src/elements/Home/ProductSection.tsx
@@ -459,7 +459,7 @@ const ProductSection = () => {
                                         {item.rewardCoins && (
                                             <div
                                                 className="badge bg-warning text-white position-absolute shadow-sm"
-                                                style={{ bottom: "60px", right: "10px", zIndex: 2, fontSize: "0.9rem" }}
+                                                style={{ bottom: "60px", right: "10px", zIndex: 2, fontSize: "0.7rem" }}
                                             >
                                                 You Will <br />Earn {item.rewardCoins}<br />  Coins
                                             </div>
